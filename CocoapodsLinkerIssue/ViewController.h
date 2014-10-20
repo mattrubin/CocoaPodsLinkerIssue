ed49e97693c2f2e69c329b8abd2da9269f7dbf85
Matt Rubin <opensource@mattrubin.me>
2014-10-20 11:51:28 -0700

    New project

---
+++ CocoapodsLinkerIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoapodsLinkerIssue
+//
+//  Created by Matt Rubin on 10/20/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
