1591cb4e5975444ac9d8b50d88f14b1e5387e163
Matt Rubin <opensource@mattrubin.me>
2014-10-20 13:20:49 -0700

    Add Library target

---
+++ Library/Library.m
@@ -0,0 +1,13 @@
+//
+//  Library.m
+//  Library
+//
+//  Created by Matt Rubin on 10/20/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import "Library.h"
+
+@implementation Library
+
+@end
